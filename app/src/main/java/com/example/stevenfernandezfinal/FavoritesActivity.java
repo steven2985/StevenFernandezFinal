960380037f2e4763d35697d149d24578851db92b
steven2985 <47422305+steven2985@users.noreply.github.com>
2021-04-26 00:02:26 -0400

    FavoritesActivity

--- app/src/main/java/com/example/stevenfernandezfinal/FavoritesActivity.java
+++ app/src/main/java/com/example/stevenfernandezfinal/FavoritesActivity.java
@@ -1,4 +1,70 @@
 package com.example.stevenfernandezfinal;
 
-public class FavoritesActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class FavoritesActivity extends AppCompatActivity {
+
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle t;
+    private NavigationView nv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorites);
+
+
+        dl = (DrawerLayout)findViewById(R.id.activity_favorites);
+        t = new ActionBarDrawerToggle(this, dl,R.string.Open, R.string.Close);
+
+        dl.addDrawerListener(t);
+        t.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        nv = (NavigationView)findViewById(R.id.nv);
+        nv.setNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+            switch(id)
+            {
+                case R.id.home:
+                    Intent home = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(home);
+                    break;
+                case R.id.login:
+                    Intent login = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(login);
+                    break;
+                case R.id.search:
+                    Intent search = new Intent(getApplicationContext(), SearchActivity.class);
+                    startActivity(search);
+                    break;
+                case R.id.favorites:
+                    break;
+                default:
+                    return true;
+            }
+
+
+            return true;
+
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(t.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
+    }
 }
