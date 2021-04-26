ff232a18bb1d8d9b1cba9bf4db73362147c4b0b9
steven2985 <47422305+steven2985@users.noreply.github.com>
2021-04-26 00:10:33 -0400

    MarketActivity

--- app/src/main/java/com/example/stevenfernandezfinal/MarketActivity.java
+++ app/src/main/java/com/example/stevenfernandezfinal/MarketActivity.java
@@ -1,4 +1,115 @@
 package com.example.stevenfernandezfinal;
 
-public class MarketActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle t;
+    private NavigationView nv;
+
+    private final String[] stocks = {"IBM","AAPL","GOOGL","AMZN","TSLA"};
+    private ArrayList<Stock> theStocks;
+    RecyclerView example;
+    StockRecyclerAdapter exampleAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        dl = (DrawerLayout)findViewById(R.id.activity_login);
+        t = new ActionBarDrawerToggle(this, dl,R.string.Open, R.string.Close);
+
+        dl.addDrawerListener(t);
+        t.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        nv = (NavigationView)findViewById(R.id.nv);
+        nv.setNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+            switch(id)
+            {
+                case R.id.home:
+                    Intent home = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(home);
+                    break;
+                case R.id.login:
+                    break;
+                case R.id.search:
+                    Intent search = new Intent(getApplicationContext(), SearchActivity.class);
+                    startActivity(search);
+                    break;
+                case R.id.favorites:
+                    Intent fav = new Intent(getApplicationContext(), FavoritesActivity.class);
+                    startActivity(fav);
+                    break;
+                default:
+                    return true;
+            }
+
+
+            return true;
+
+        });
+        theStocks = new ArrayList<Stock>();
+        example = findViewById(R.id.recycler);
+
+        for (String s : stocks) {
+            String url = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={{YV0CX9FB67PQCCJD}}&apikey= {}".replace("{{YV0CX9FB67PQCCJD}}", s);
+            RequestQueue queue = Volley.newRequestQueue(this);
+
+
+            GsonRequest<GlobalQuote> req = new GsonRequest<GlobalQuote>(url, GlobalQuote.class, null, new Response.Listener<GlobalQuote>() {
+                @Override
+                public void onResponse(GlobalQuote response) {
+
+                    System.out.println("Successful Response");
+                    System.out.println(response.getGlobalQuote().getSymbol());
+                    theStocks.add(response.getGlobalQuote());
+
+                    if (theStocks.size() == stocks.length) {
+                        doView();
+                    }
+
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    System.out.println("That didn't work!");
+                }
+            });
+
+            queue.add(req);
+        }
+    }
+
+    private void doView(){
+        exampleAdapter = new StockRecyclerAdapter(theStocks);
+        example.setAdapter(exampleAdapter);
+        example.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(t.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
+    }
 }
