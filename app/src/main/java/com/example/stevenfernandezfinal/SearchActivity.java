9befe2dcd9dbd731f7748948f1b53d29250612bb
steven2985 <47422305+steven2985@users.noreply.github.com>
2021-04-26 00:11:40 -0400

    SearchActivity

--- app/src/main/java/com/example/stevenfernandezfinal/SearchActivity.java
+++ app/src/main/java/com/example/stevenfernandezfinal/SearchActivity.java
@@ -1,4 +1,79 @@
 package com.example.stevenfernandezfinal;
 
-public class SearchActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
+
+public class SearchActivity extends AppCompatActivity {
+
+    private final String stock = null;
+    private ArrayList<Stock> theStocks;
+    RecyclerView example;
+    StockRecyclerAdapter exampleAdapter;
+
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle t;
+    private NavigationView nv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        dl = (DrawerLayout)findViewById(R.id.activity_search);
+        t = new ActionBarDrawerToggle(this, dl,R.string.Open, R.string.Close);
+
+        example = findViewById(R.id.recycler);
+
+        dl.addDrawerListener(t);
+        t.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        nv = (NavigationView)findViewById(R.id.nv);
+        nv.setNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+            switch(id)
+            {
+                case R.id.home:
+                    Intent home = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(home);
+                    break;
+                case R.id.login:
+                    Intent login = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(login);
+                    break;
+                case R.id.search:
+                    break;
+                case R.id.favorites:
+                    Intent fav = new Intent(getApplicationContext(), FavoritesActivity.class);
+                    startActivity(fav);
+                    break;
+                default:
+                    return true;
+            }
+
+
+            return true;
+
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(t.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
+    }
 }
